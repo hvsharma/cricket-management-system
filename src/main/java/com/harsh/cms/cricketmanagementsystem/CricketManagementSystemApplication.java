1936e307904408cac6024ac25e21c028bcc1d222
Harsh Vardhan Sharma <hvsharma_learning@outlook.com>
2017-11-08 09:54:54 +0530

    Initialized!

---
+++ src/main/java/com/harsh/cms/cricketmanagementsystem/CricketManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.harsh.cms.cricketmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketManagementSystemApplication.class, args);
+	}
+}
