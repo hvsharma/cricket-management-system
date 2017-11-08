a824ebed0fb7fa25c70e140faaa953f829cd9486
Harsh Vardhan Sharma <hvsharma_learning@outlook.com>
2017-11-08 11:22:32 +0530

    Initialized

--- src/main/java/com/harsh/cms/CmsApplication.java
+++ src/main/java/com/harsh/cms/CmsApplication.java
@@ -1,12 +1,12 @@
-package com.harsh.cms.cricketmanagementsystem;
+package com.harsh.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CricketManagementSystemApplication {
+public class CmsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CricketManagementSystemApplication.class, args);
+		SpringApplication.run(CmsApplication.class, args);
 	}
 }
